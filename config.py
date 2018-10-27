a4504beb38113140fdfe1d02a56164ada88f13d2
zevloy <liuyang5599@163.com>
2018-10-27 05:34:48 -0400

    minor change

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ def get_conf():
 def get_stcPacket_conf():
     '''fetch the parameters used by stcPacket layer, such as "custom_pattern"'''
 
-    init_conf("StcConf/case91_p1_tx_traffic_config.xml")
+    init_conf("config/case91_p1_tx_traffic_config.xml")
 
     global traffic_config
 
@@ -71,7 +71,7 @@ def get_stcPacket_conf():
 
 if __name__ == '__main__':
 
-    init_conf("StcConf/case91_p1_tx_traffic_config.xml")
+    init_conf("config/case91_p1_tx_traffic_config.xml")
     print get_conf()
 
     custom_pattern, signature_length, payload_length, custom_pattern_length, padding_length = get_stcPacket_conf()
